8f6ce5f415951b4f7426fc477a077b5b69953219
joeyklee <jsphknglee@gmail.com>
2019-03-14 11:19:48 -0400

    added emojis for user profiles

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -8,24 +8,39 @@ module.exports = function (app) {
     email: {
       type: String,
       unique: true,
+      required:true,
       lowercase: true
     },
     password: {
-      type: String
+      type: String,
+      required:true,
     },
     username:{
       type:String,
       unique:true,
+      required:true
     },
     profile_image: {
       type: String
     },
     bio: {
-      type: String
+      type: String,
+      required:true,
+      default:'Hi!'
     },
     githubId: {
       type: String
     },
+    selectedEmoji: {
+      type: Number,
+      default: () => Math.floor(Math.random() * 11),
+      required:true
+    },
+    emojis: {
+          type: Array,
+          default: ['BEAR-1F43B.png','CHICKEN-1F414.png','COW-1F42E.png','DOG-1F436.png','FROG-1F438.png','HAMSTER-1F439.png','MONKEY-1F435.png','MOUSE-1F42D.png','PANDA-1F43C.png','PIG-1F437.png','TIGER-1F42F.png','WOLF-1F43A.png'],
+          required: true
+      },
   }, {
     timestamps: true
   });
