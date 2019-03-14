8fef21897726cc59fb51cc35ecc6cc23ae3d4eaf
joeyklee <jsphknglee@gmail.com>
2019-03-14 18:30:35 -0400

    added collections model and populate

--- src/models/collections.model.js
+++ src/models/collections.model.js
@@ -1,21 +1,52 @@
+const shortid = require('shortid');
+const generate = require('project-name-generator');
 // collections-model.js - A mongoose model
 // 
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
+  const {
+    Schema
+  } = mongooseClient;
   const collections = new Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    owner: {
-      type: String, required: true
+    _id: {
+      type: String,
+      default: shortid.generate
     },
-    projects:[{
+    alias: {
+      type: String,
+      default: generate({
+        words: 2,
+        alliterative: true
+      }).dashed
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: false
+    },
+    owner: {
       type: Schema.Types.ObjectId,
+      ref: 'users',
+    },
+    projects: [{
+      type: String,
       required: false,
-      ref: 'projects'
-    }], // // reference to projects id
+      ref: 'projects',
+    }], // // reference to projects id,
+    selectedColor: {
+      type: Number,
+      default: () => Math.floor(Math.random() * 5)
+    },
+    colors: {
+          type: Array,
+          default: ["#FF725C", "#FFD700", "#FF80CC", "#9EEBCF", "#CDECFF", "#A463F2"],
+          required: true
+      },
   }, {
     timestamps: true
   });
--- src/services/collections/collections.hooks.js
+++ src/services/collections/collections.hooks.js
@@ -1,18 +1,42 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const checkUser = require('../../hooks/check-user')
+const addOwner = require('../../hooks/add-owner')
+const { populate } = require('feathers-hooks-common');
+
+const userPopulateSchema = {
+  include: {
+    service: 'users',
+    nameAs: 'ownerDetails',
+    parentField: 'owner',
+    childField: '_id',
+    query:{
+      $select: ['username', '_id'],
+    }
+  }
+};
+
+const projectPopulateSchema = {
+  include: {
+    service: '/api/projects',
+    nameAs: 'projectsDetails',
+    parentField: 'projects',
+    childField: '_id',
+  }
+};
 
 module.exports = {
   before: {
     all: [ ],
     find: [],
     get: [],
-    create: [authenticate('jwt')],
+    create: [authenticate('jwt'), addOwner()],
     update: [authenticate('jwt')],
     patch: [authenticate('jwt')],
     remove: [authenticate('jwt')]
   },
 
   after: {
-    all: [],
+    all: [populate({schema:userPopulateSchema}), populate({schema:projectPopulateSchema}) ],
     find: [],
     get: [],
     create: [],
